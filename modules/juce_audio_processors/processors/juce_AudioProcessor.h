f1e48ace25d2ca26ddca5b935e78092dc79735f4
JP Cimalando <jpcima@users.noreply.github.com>
2018-05-13 23:08:04 +0200

    add LV2 plugin client

--- modules/juce_audio_processors/processors/juce_AudioProcessor.h
+++ modules/juce_audio_processors/processors/juce_AudioProcessor.h
@@ -1192,7 +1192,8 @@ public:
         wrapperType_RTAS,
         wrapperType_AAX,
         wrapperType_Standalone,
-        wrapperType_Unity
+        wrapperType_Unity,
+        wrapperType_LV2,
     };
 
     /** When loaded by a plugin wrapper, this flag will be set to indicate the type
